722931c592cb328426af011ba860d099ec8f7279
francoglov <franco.glovasa@intentgine.com>
2022-12-20 23:33:40 +0800

    video meta data -test.chtml

--- VideoToThumbsAngularJS/Controllers/HomeController.cs
+++ VideoToThumbsAngularJS/Controllers/HomeController.cs
@@ -22,6 +22,20 @@ namespace VideoToThumbsAngularJS.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+
+        public IActionResult VThumb()
+        {
+            return View();
+        }
+        public IActionResult Yakobo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
